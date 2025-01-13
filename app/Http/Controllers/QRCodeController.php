3e8fa373c3ee6f94d3ca3d99a23b0a0eb408ba0a
dkAsCambodia <nitindamo50@gmail.com>
2025-01-13 17:06:18 +0700

    set customer name column in generate qr form

--- app/Http/Controllers/QRCodeController.php
+++ app/Http/Controllers/QRCodeController.php
@@ -15,9 +15,11 @@ class QRCodeController extends Controller
     public function generateQRCode(Request $request)
     {
         $validator = Validator::make($request->all(), [
+            'customer_name' => 'required',
             'amount' => ['required', 'numeric', 'min:300.00'],
             'invoice_number' => ['required', 'regex:/^[a-zA-Z0-9\-\/#]+$/', Rule::unique('payment_details', 'transaction_id'),],
         ], [
+            'customer_name.required' => 'Customer name is required.',
             'amount.required' => 'Amount is required.',
             'amount.numeric' => 'Amount must be a valid number.',
             'amount.min' => 'Amount must be 300.00 or greater.',
@@ -29,11 +31,12 @@ class QRCodeController extends Controller
         if ($validator->fails()) {
             return redirect()->back()->withErrors($validator)->withInput();
         }
+        $customer_name=$request->customer_name;
         $amount=$request->amount;
         $invoice_number=$request->invoice_number;
 
-        // $url = 'http://localhost/payin/FCdeposit/deposit.php?aa='.base64_encode($amount).'&in='.base64_encode($invoice_number); 
-        $url = 'https://payin.implogix.com/FCdeposit/deposit.php?aa='.base64_encode($amount).'&in='.base64_encode($invoice_number); 
+        // $url = 'http://localhost/payin/FCdeposit/deposit.php?aa='.base64_encode($amount).'&in='.base64_encode($invoice_number).'&cu='.base64_encode($customer_name); 
+        $url = 'https://payin.implogix.com/FCdeposit/deposit.php?aa='.base64_encode($amount).'&in='.base64_encode($invoice_number).'&cu='.base64_encode($customer_name); 
 
         return view('showQR', compact('url','amount','invoice_number'));
     }
