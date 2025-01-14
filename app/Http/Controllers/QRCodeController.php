1d00066d4d580a7b00cc0a113515bea0864dc1af
dkAsCambodia <nitindamo50@gmail.com>
2025-01-14 17:50:57 +0700

    insert data and show the generated qr code list in dashboard

--- app/Http/Controllers/QRCodeController.php
+++ app/Http/Controllers/QRCodeController.php
@@ -115,7 +115,7 @@ class QRCodeController extends Controller
                             <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title">' . trans("messages.detail") . '</h5>
+                                        <h5 class="modal-title">' . trans("messages.View") . '</h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
