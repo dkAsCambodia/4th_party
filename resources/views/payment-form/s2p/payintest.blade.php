00723f842b86bfbf5e643e6e95749a289358a16c
dkAsCambodia <nitindamo50@gmail.com>
2025-01-24 18:09:29 +0700

    integrate s2p webhook callback in laravel

--- resources/views/payment-form/s2p/payintest.blade.php
+++ resources/views/payment-form/s2p/payintest.blade.php
@@ -24,36 +24,6 @@ $data = [
 ];
 $fullUrl = $apiUrl . '?' . http_build_query($data);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Processing...</title>
-    <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: Arial, sans-serif;
-            background: #000;
-        }
-        .loading-container { text-align: center; }
-        .loading-container img { width: 100px; height: 100px;}
-        p{ color:white; }
-    </style>
-</head>
-<body>
-    <div class="loading-container">
-        <img src="https://i.gifer.com/ZZ5H.gif" alt="Loading...">
-        <p>Processing, please wait...</p>
-    </div>
-    <script>
-        setTimeout(() => {
-            window.location.href = '<?php echo $fullUrl; ?>';
-        }, 2000); // 2-second delay for demo purposes (optional)
-    </script>
-</body>
-</html>
+<script>
+    window.location.href = '<?php echo $fullUrl; ?>';
+</script>
