c3ca01dc16e252cdffeaf3d968df3ae40c02b0f5
dkAsCambodia <nitindamo50@gmail.com>
2025-02-15 11:39:06 +0700

    create testing url form h2p withdrawal

--- resources/views/payment-form/h2p/payouttest.blade.php
+++ resources/views/payment-form/h2p/payouttest.blade.php
@@ -8,14 +8,14 @@ $referenceNo = "GZTRN" . time() . (function ($length = 3) {
     }
     return $randomString;
 })();
-$apiUrl = "http://127.0.0.1:8000/api/h2p/payout";
-// $apiUrl = "https://payment.implogix.com/api/h2p/payout";
+// $apiUrl = "http://127.0.0.1:8000/api/h2p/payout";
+$apiUrl = "https://payment.implogix.com/api/h2p/payout";
 $data = [
     'merchant_code' => $_GET['merchant_code'], 
     'product_id' => '8',
     'referenceId' => $referenceNo, 
-    'callback_url' => 'http://127.0.0.1:8000/api/h2p/payout/callbackURL',
-    // 'callback_url' => 'https://payment.implogix.com/api/h2p/payout/callbackURL',
+    // 'callback_url' => 'http://127.0.0.1:8000/api/h2p/payout/callbackURL',
+    'callback_url' => 'https://payment.implogix.com/api/h2p/payout/callbackURL',
     'Currency' => $_GET['Currency'], 
     'amount' => $_GET['amount'], 
     'customer_name' => $_GET['customer_name'], 
